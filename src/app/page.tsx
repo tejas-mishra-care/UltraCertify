b6eeb84deffd7282a63b815e0b8eaefc2f8fbf56
Tejas Mishra <tejas.mishra.care@gmail.com>
2025-09-22 17:10:27 +0000

    Build Error
    Parsing ecmascript source code failed
    
    ./src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -458,7 +458,6 @@ const UltraCertifyPage: FC = () => {
     }, 1500);
   }
 
-
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2 space-y-6">
